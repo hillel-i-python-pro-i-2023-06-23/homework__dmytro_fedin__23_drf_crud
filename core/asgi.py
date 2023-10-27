8978789684eccdedf9c72fac57ee5a068590b375
dafedin <tm.i.sm.master@gmail.com>
2023-10-27 16:26:08 +0200

    App installed

--- core/asgi.py
+++ core/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homework__dmytro_fedin__23_drf_crud.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homework__dmytro_fedin__23_drf_crud.settings")
 
 application = get_asgi_application()
--- core/wsgi.py
+++ core/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homework__dmytro_fedin__23_drf_crud.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homework__dmytro_fedin__23_drf_crud.settings")
 
 application = get_wsgi_application()
